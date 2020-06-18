5197bad895d87284afbd0d50064273351da5ac19
Phoebe Law <phoebecodes@gmail.com>
2020-06-18 16:02:15 -0700

    adds event type inclusion validation in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ApplicationRecord
-  validates :name, :event_type, presence: true
-  # validates :name, presence: true
-  # validates :event_type, inclusion: ["click", "view", "scroll", "play", "download", "refresh", "submit"]
+  validates :name, presence: true
+  validates :event_type, inclusion: { in: ["click", "view", "scroll", "play", "download", "refresh", "submit"]}
 end
