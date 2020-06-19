14163e4f659fe23b5607c49196827a276c7e3ab6
Derrek Long <derrek.long@gmail.com>
2020-06-18 17:32:13 -0700

    adds index routing to event

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :events, only: [:create]
+  resources :events, only: [:create, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
